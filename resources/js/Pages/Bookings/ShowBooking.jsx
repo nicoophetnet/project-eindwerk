80225ae71b06265f555b42a217d06af1ede8eaf3
Nico Couwberghs <nico.couwberghs@gmail.com>
2023-05-13 14:47:55 +0200

    created ShowBooking component

--- resources/js/Pages/Bookings/ShowBooking.jsx
+++ resources/js/Pages/Bookings/ShowBooking.jsx
@@ -1,3 +1,37 @@
-export default function YourBookings() {
-    return <div>Your bookings</div>;
-}
+import React from "react";
+import { InertiaLink } from "@inertiajs/inertia-react";
+
+const ShowBooking = ({ booking }) => {
+    const { flight, passengers } = booking;
+
+    return (
+        <div>
+            <h1>Booking #{booking.id}</h1>
+
+            <h2>Flight Information</h2>
+            <p>Flight ID: {flight.id}</p>
+            <p>Departure Airport: {flight.departure_airport}</p>
+            <p>Arrival Airport: {flight.arrival_airport}</p>
+            <p>Departure Time: {flight.departure_time}</p>
+            <p>Arrival Time: {flight.arrival_time}</p>
+
+            <h2>Passenger Information</h2>
+            {passengers.map((passenger, index) => (
+                <div key={index}>
+                    <h3>Passenger {index + 1}</h3>
+                    <p>
+                        Name: {passenger.first_name} {passenger.last_name}
+                    </p>
+                    <p>Phone Number: {passenger.phone_number}</p>
+                    <p>Email: {passenger.email}</p>
+                </div>
+            ))}
+
+            <p>
+                <InertiaLink href="/">Back to Home</InertiaLink>
+            </p>
+        </div>
+    );
+};
+
+export default ShowBooking;
